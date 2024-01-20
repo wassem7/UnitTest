0038651adb46f9a2821d4a0d4f314f0e6c01b4d8
wassem7 <wassemdarkwa@gmail.com>
2024-01-20 13:11:57 +0000

    NUnit Test

---
+++ SparkyMSTest/CalculatorMSTests.cs
@@ -0,0 +1,20 @@
+﻿using Sparky;
+
+namespace SparkyMSTest;
+
+[TestClass]
+public class CalculatorMsTest
+{
+    [TestMethod]
+    public void Test_AddNumbers_TwoInts_GetCorrectResults()
+    {
+        //Arrange
+
+        Calculator calc = new();
+        //Act
+        var result = calc.AddNumbers(10, 20);
+        //Assert
+
+        Assert.AreEqual(30, result);
+    }
+}
---
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Sparky;
+
+[TestFixture]
+public class CalculatorNUnitTests
+{
+    [Test]
+    public void AddNumbers_TwoInts_GetCorrectAnswer()
+    {
+        //Arrange
+        Calculator calculator = new();
+        //Act
+        var result = calculator.AddNumbers(10, 20);
+        //Assert
+
+        Assert.AreEqual(30, result);
+    }
+}
